de2df7ff9c71b758a928d8dd2682151774f0f523
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2023-02-09 13:17:47 -0300

    Create directory if necessary

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -2,6 +2,8 @@ import logging
 from pathlib import Path
 
 LOG_DIR: Path = Path("../logs/")
+LOG_DIR.mkdir(exist_ok=True, parents=True)
+
 LOG_PATH: Path = LOG_DIR / "web_scrape.log"
 
 logging.basicConfig(
