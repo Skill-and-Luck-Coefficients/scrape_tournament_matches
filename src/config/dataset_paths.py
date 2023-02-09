930372ada1bfc04a4305b1c2da69c013e4a306a1
EstefanoB <48600696+EstefanoB@users.noreply.github.com>
2023-02-09 13:16:27 -0300

    Fix string concatenation bug

--- src/config/dataset_paths.py
+++ src/config/dataset_paths.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 DATA_PATH = Path("../data/")
 
-SCRAPE_PATH: Path = DATA_PATH + "bet_explorer/"
-FORMAT_PATH: Path = DATA_PATH + "formatted/"
+SCRAPE_PATH: Path = DATA_PATH / "bet_explorer/"
+FORMAT_PATH: Path = DATA_PATH / "formatted/"
